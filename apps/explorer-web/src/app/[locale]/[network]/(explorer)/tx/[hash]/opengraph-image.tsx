03bb6d339c50f51212dbd4f21cc92e4b20c7a675
Miguel Nieto <miguelnietoarias3@gmail.com>
2026-07-14 21:26:46 -0500

    Fix Prettier formatting in OpenGraph image components (#39)

--- apps/explorer-web/src/app/[locale]/[network]/(explorer)/tx/[hash]/opengraph-image.tsx
+++ apps/explorer-web/src/app/[locale]/[network]/(explorer)/tx/[hash]/opengraph-image.tsx
@@ -154,7 +154,9 @@ export default async function Image({ params }: { params: { hash: string; locale
             <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
           </svg>
         </div>
-        <span style={{ color: "rgba(255, 255, 255, 0.5)", fontSize: 18 }}>StellarView Explorer</span>
+        <span style={{ color: "rgba(255, 255, 255, 0.5)", fontSize: 18 }}>
+          StellarView Explorer
+        </span>
       </div>
 
       {/* Bottom gradient */}
